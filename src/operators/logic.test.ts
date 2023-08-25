4c55a7924e6bfd75feab9381a550b6c683d630ea
MarcLlobet <marc.llobet@typeform.com>
2023-08-26 01:56:29 +0200

    feat: add more operators with tests

--- src/operators/logic.test.ts
+++ src/operators/logic.test.ts
@@ -41,4 +41,84 @@ describe('operators logic', () => {
       expect(mockCallback).not.toHaveBeenCalled()
     })
   })
+
+  describe('WHILE', () => {
+    beforeEach(() => {
+      jest.resetAllMocks()
+    })
+    it('callback is triggered as many times as number passed', () => {
+      let value = 0
+      
+      const mockCallback = jest.fn(() => {value += 1})
+      const conditionFunction = () => value < 5
+      operators.WHILE(conditionFunction, mockCallback)
+      expect(mockCallback).toHaveBeenCalledTimes(5)
+    })
+  })
+
+  describe('FOR_LOOP', () => {
+    beforeEach(() => {
+      jest.resetAllMocks()
+    })
+    it('callback is triggered as many times as number passed', () => {
+      const mockCallback = jest.fn()
+      operators.FOR_LOOP(5, mockCallback)
+      expect(mockCallback).toHaveBeenCalledTimes(5)
+    })
+  })
+
+  describe('GREATER_THAN', () => {
+    it('returns true if greater', () => {
+      expect(operators.GREATER_THAN(5, 4)).toBe(true)
+    })
+
+    it('returns false if equal', () => {
+      expect(operators.GREATER_THAN(5, 5)).toBe(false)
+    })
+
+    it('returns false if lower', () => {
+      expect(operators.GREATER_THAN(4, 5)).toBe(false)
+    })
+  })
+  describe('GREATER_OR_EQUAL_THAN', () => {
+    it('returns true if greater', () => {
+      expect(operators.GREATER_OR_EQUAL_THAN(5, 4)).toBe(true)
+    })
+
+    it('returns true if equal', () => {
+      expect(operators.GREATER_OR_EQUAL_THAN(5, 5)).toBe(true)
+    })
+
+    it('returns false if lower', () => {
+      expect(operators.GREATER_OR_EQUAL_THAN(4, 5)).toBe(false)
+    })
+  })
+
+  describe('LOWER_THAN', () => {
+    it('returns true if lower', () => {
+      expect(operators.LOWER_THAN(4, 5)).toBe(true)
+    })
+
+    it('returns false if equal', () => {
+      expect(operators.LOWER_THAN(5, 5)).toBe(false)
+    })
+
+    it('returns false if greater', () => {
+      expect(operators.LOWER_THAN(5, 4)).toBe(false)
+    })
+  })
+
+  describe('LOWER_OR_EQUAL_THAN', () => {
+    it('returns true if lower', () => {
+      expect(operators.LOWER_OR_EQUAL_THAN(4, 5)).toBe(true)
+    })
+
+    it('returns true if equal', () => {
+      expect(operators.LOWER_OR_EQUAL_THAN(5, 5)).toBe(true)
+    })
+
+    it('returns false if greater', () => {
+      expect(operators.LOWER_OR_EQUAL_THAN(5, 4)).toBe(false)
+    })
+  })
 })
--- src/operators/logic.ts
+++ src/operators/logic.ts
@@ -6,11 +6,22 @@ export const operators: Record<OperatorNameKey, Function> = {
       nestedCallback()
     }
   },
-  WHILE: (condition: boolean, nestedCallback: Function = () => {}) => {
-      while(condition){
+  WHILE: (condition: Function, nestedCallback: Function = () => {}) => {
+      while(condition()){
         nestedCallback()
       }
   },
+  FOR_LOOP: (times: number, nestedCallback: Function): void => {
+    let i = 1
+    for(i; i <= times; i+=1){
+      nestedCallback()
+    }
+  },
   IS_EQUAL: (value1: any, value2: any): boolean => value1 === value2,
   IS_NOT_EQUAL: (value1: any, value2: any): boolean => value1 !== value2,
+  GREATER_THAN: (value1: any, value2: any): boolean => value1 > value2,
+  GREATER_OR_EQUAL_THAN: (value1: any, value2: any): boolean => value1 >= value2,
+  LOWER_THAN: (value1: any, value2: any): boolean => value1 < value2,
+  LOWER_OR_EQUAL_THAN: (value1: any, value2: any): boolean => value1 <= value2,
+  
 }
