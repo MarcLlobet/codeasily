1a161143fe99b99637ee2e1f4d821e42bb1d9f36
MarcLlobet <marc.llobet@typeform.com>
2023-08-26 01:13:24 +0200

    test: add some operator logic tests

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,24 @@
 import React, { useEffect } from 'react';
 import './App.css';
 import { useVariables } from './context/variables';
+import { operators } from './operators';
 
 function App() {
   const {variables, setVariables} = useVariables()
 
   useEffect(() => {
-    setVariables?.({'dos': 2})
-  }, [])
+    setVariables?.({UN: 1, DOS: 2, TRES: 3, SEGON_TRES: 3})
+  }, []) // eslint-disable-line react-hooks/exhaustive-deps
 
-    console.log({variables})
+    console.log({
+      variables,
+      is: operators.IS_EQUAL(variables.TRES, variables.SEGON_TRES),
+      isNot: operators.IS_NOT_EQUAL(variables.TRES, variables.DOS),
+      if: operators.IF(
+        operators.IS_EQUAL(variables.TRES, variables.SEGON_TRES), 
+        console.log(variables.DOS)
+      )
+    })
     
   return (
     <div>A</div>
